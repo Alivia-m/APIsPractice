4b70416b0043eda120a65358c7df481e5fc8fb4a
Alivia-m <aliviagayle@icloud.com>
2021-07-15 19:23:32 -0500

    exercise complete

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace APIsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonSwansonQuote();
+
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonSwansonQuote();
+
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonSwansonQuote();
+
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonSwansonQuote();
+
+            QuoteGenerator.KanyeQuote();
+
+            QuoteGenerator.RonSwansonQuote();
+
+        }
+    }
+}
